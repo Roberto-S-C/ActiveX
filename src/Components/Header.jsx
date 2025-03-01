40ccf553871d8d08e387fe0fe0ab07308e42b2d5
Roberto-S-C <robertosanchezcazares@gmail.com>
2025-03-01 12:14:24 -0600

    Product List reload

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,14 +1,21 @@
 import { ChevronLeftIcon } from '@heroicons/react/24/solid'
-import React from 'react'
+import React, { useContext } from 'react'
 import { useNavigate } from 'react-router'
+import getProducts from '../scripts/getProducts'
+import { ProductListContext } from '../main'
 
 function Header({ title }) {
 
     const navigate = useNavigate()
+    const { productList, setProductList } = useContext(ProductListContext);
 
     return (
         <div className='flex justify-center items-center relative bg-red-600'>
-            <button onClick={() => navigate('/products')} className='absolute left-0'>
+            <button onClick={() => {
+                getProducts()
+                    .then(data => setProductList(data))
+                navigate('/products')
+            }} className='absolute left-0'>
                 <ChevronLeftIcon className='h-8 w-8 text-white font-bold hover:text-slate-200' />
             </button>
             <h1 className='text-white text-3xl font-bold p-3'>{title}</h1>
