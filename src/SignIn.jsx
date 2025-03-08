120ad65d37bd26e1089f654859ab8c2f5964ffc1
Roberto-S-C <robertosanchezcazares@gmail.com>
2025-03-08 14:03:25 -0600

    Responsive Authentication

--- src/SignIn.jsx
+++ src/SignIn.jsx
@@ -7,6 +7,7 @@ import { Link, useNavigate } from 'react-router'
 import { useCookies } from 'react-cookie'
 import signIn from './scripts/User/signIn'
 import Alert from './Components/Alert'
+import Header from './Components/Header'
 
 function SignIn() {
     const { register, handleSubmit, formState: { errors } } = useForm({ defaultValues: { username: '', email: '', password: '' } })
@@ -42,69 +43,73 @@ function SignIn() {
 
 
     return (
-        <div className='flex flex-col justify-center items-center container mx-auto h-screen'>
+        <div>
+            <Header title={'Sign In'} />
+            <div className='flex flex-col justify-center items-center h-screen'>
 
-            {showAlert && <Alert
-                alertDetails={alertDetails}
-                showAlert={showAlert}
-                setShowAlert={setShowAlert}
-            />}
+                {showAlert && <Alert
+                    alertDetails={alertDetails}
+                    showAlert={showAlert}
+                    setShowAlert={setShowAlert}
+                />}
 
-            <div className='flex flex-col items-center w-1/3 h-3/5 p-4 rounded-lg bg-slate-200'>
-                <div className='flex flex-col justify-center items-center w-full h-1/3'>
-                    <img src={logo} className='w-1/6' />
-                    <h2 className='text-red-600 text-2xl font-bold'>Sign In</h2>
-                </div>
+                <div className='flex flex-col items-center w-4/5 md:w-3/5 lg:w-2/5 h-3/5 p-4 rounded-lg bg-slate-200'>
+                    <div className='flex flex-col justify-center items-center w-full h-1/3'>
+                        <img src={logo} className='w-1/6' />
+                        <h2 className='text-red-600 text-2xl font-bold'>ActiveX</h2>
+                    </div>
 
-                <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col justify-around w-full h-2/3'>
-                    <input type='text' placeholder='Username' name='username'
-                        {...register('username', { required: true, maxLength: 50 })}
-                        aria-invalid={errors.username ? "true" : "false"}
-                        className='w-full p-2 my-2 border border-gray-300 rounded-lg' />
-                    {errors.username?.type === 'required' && <p role="alert" className='text-red-600 pl-2'>Username is required</p>}
-
-                    <div className='relative flex items-center'>
-                        <input type={inputType} placeholder='Password' name='password'
-                            {...register('password', {
-                                required: 'Password is required',
-                                minLength: {
-                                    value: 8,
-                                    message: 'Password must be at least 8 characters'
-                                },
-                                pattern: {
-                                    value: /^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,}$/,
-                                    message: 'Password must contain 1 uppercase letter, 1 number, and 1 special character'
-                                }
-                            })}
-                            aria-invalid={errors.password ? "true" : "false"}
+                    <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col justify-around w-full h-2/3'>
+                        <input type='text' placeholder='Username' name='username'
+                            {...register('username', { required: true, maxLength: 50 })}
+                            aria-invalid={errors.username ? "true" : "false"}
                             className='w-full p-2 my-2 border border-gray-300 rounded-lg' />
-                        {showPassword ? (
-                            <EyeSlashIcon onClick={() => {
-                                setShowPassword(false)
-                                setInputType('password')
-                            }} className='absolute right-2 size-6 text-gray-500 hover:cursor-pointer' />
-                        ) : (
-                            <EyeIcon onClick={() => {
-                                setShowPassword(true)
-                                setInputType('text')
-                            }} className='absolute right-2 size-6 text-gray-500 hover:cursor-pointer' />
+                        {errors.username?.type === 'required' && <p role="alert" className='text-red-600 pl-2'>Username is required</p>}
+
+                        <div className='relative flex items-center'>
+                            <input type={inputType} placeholder='Password' name='password'
+                                {...register('password', {
+                                    required: 'Password is required',
+                                    minLength: {
+                                        value: 8,
+                                        message: 'Password must be at least 8 characters'
+                                    },
+                                    pattern: {
+                                        value: /^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,}$/,
+                                        message: 'Password must contain 1 uppercase letter, 1 number, and 1 special character'
+                                    }
+                                })}
+                                aria-invalid={errors.password ? "true" : "false"}
+                                className='w-full p-2 my-2 border border-gray-300 rounded-lg' />
+                            {showPassword ? (
+                                <EyeSlashIcon onClick={() => {
+                                    setShowPassword(false)
+                                    setInputType('password')
+                                }} className='absolute right-2 size-6 text-gray-500 hover:cursor-pointer' />
+                            ) : (
+                                <EyeIcon onClick={() => {
+                                    setShowPassword(true)
+                                    setInputType('text')
+                                }} className='absolute right-2 size-6 text-gray-500 hover:cursor-pointer' />
+                            )}
+
+                        </div>
+                        {errors.password && (
+                            <p role="alert" className='text-red-600 pl-2'>
+                                {errors.password.message}
+                            </p>
                         )}
 
-                    </div>
-                    {errors.password && (
-                        <p role="alert" className='text-red-600 pl-2'>
-                            {errors.password.message}
-                        </p>
-                    )}
+                        <div className='flex justify-center'>
+                            <button type='submit' className='bg-red-600 w-2/3 md:w-1/3 lg:w-1/4 p-2 rounded-lg text-white text-lg hover:bg-red-700 hover:cursor-pointer'>Submit</button>
+                        </div>
+                    </form>
+                </div>
 
-                    <div className='flex justify-center'>
-                        <button type='submit' className='bg-red-600 w-1/4 p-2 rounded-lg text-white text-lg hover:bg-red-700 hover:cursor-pointer'>Submit</button>
-                    </div>
-                </form>
+                <p className='m-1 text-red-600'>Don't have an account? <Link to='/signup' className='underline'>Sign Up</Link></p>
             </div>
-
-            <p className='m-1 text-red-600'>Don't have an account? <Link to='/signup' className='underline'>Sign Up</Link></p>
         </div>
+
     )
 }
 
